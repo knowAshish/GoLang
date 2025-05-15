04a07b211df30c2729b810077bc116dd5a6d5da1
Ashish Patel <102802615+knowAshish@users.noreply.github.com>
2025-05-15 22:06:56 +0530

    Add files via upload

---
+++ Hello.go
@@ -0,0 +1,8 @@
+package main // This defines the package name. 'main' is special — it tells Go that this is an executable program.
+
+import "fmt" // The 'fmt' package provides I/O formatting functions like Println.
+
+func main() { // The main function is the entry point of a Go program. Execution starts here.
+
+	fmt.Println("Hello! This is Go") // This prints the string to the console followed by a new line.
+}
